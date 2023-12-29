689a82655a4f62e777bde68ca69785c70d457787
Anthony Chamberland <61058826+AnthoChamb@users.noreply.github.com>
2023-12-28 20:05:44 -0500

    Add GitHub Actions and package metadata

--- src/FsgXmk.Core/Interfaces/IO/IXmkEventReader.cs
+++ src/FsgXmk.Core/Interfaces/IO/IXmkEventReader.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace FsgXmk.Core.Interfaces.IO
 {
-    public interface IXmkEventReader : IDisposable 
+    public interface IXmkEventReader : IDisposable
     {
         IXmkEvent Read();
         Task<IXmkEvent> ReadAsync();
--- src/FsgXmk.Core/Interfaces/IO/IXmkTimeSignatureReader.cs
+++ src/FsgXmk.Core/Interfaces/IO/IXmkTimeSignatureReader.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace FsgXmk.Core.Interfaces.IO
 {
-    public interface IXmkTimeSignatureReader : IDisposable 
+    public interface IXmkTimeSignatureReader : IDisposable
     {
         IXmkTimeSignature Read();
         Task<IXmkTimeSignature> ReadAsync();
