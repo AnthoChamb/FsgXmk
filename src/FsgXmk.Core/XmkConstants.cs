94fdd3977ced1731ccc8cf4185707907ba36511b
Anthony Chamberland <anthochamberland19@outlook.com>
2024-04-17 21:58:26 -0400

    Add XmkConstants

---
+++ src/FsgXmk.Core/XmkConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace FsgXmk.Core
+{
+    public class XmkConstants
+    {
+        public const int XmkHeaderSize = 28;
+        public const int XmkTempoSize = 12;
+        public const int XmkTimeSignatureSize = 16;
+        public const int XmkEventSize = 24;
+    }
+}
