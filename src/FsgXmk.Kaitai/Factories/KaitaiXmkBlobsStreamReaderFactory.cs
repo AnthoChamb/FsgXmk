14a4097303c3912e4e5ac709796e95b0cb3acf51
Anthony Chamberland <anthochamberland19@outlook.com>
2024-12-07 18:25:17 -0500

    Add KaitaiXmkBlobsStreamReader

---
+++ src/FsgXmk.Kaitai/Factories/KaitaiXmkBlobsStreamReaderFactory.cs
@@ -0,0 +1,16 @@
+﻿using FsgXmk.Abstractions.Interfaces.Factories;
+using FsgXmk.Abstractions.Interfaces.IO;
+using FsgXmk.Kaitai.IO;
+using Kaitai;
+using System.IO;
+
+namespace FsgXmk.Kaitai.Factories
+{
+    public class KaitaiXmkBlobsStreamReaderFactory : IXmkBlobsStreamReaderFactory
+    {
+        public IXmkBlobsReader Create(Stream stream, bool leaveOpen)
+        {
+            return new KaitaiXmkBlobsStreamReader(new KaitaiStream(stream), leaveOpen);
+        }
+    }
+}
---
+++ src/FsgXmk.Kaitai/IO/KaitaiXmkBlobsStreamReader.cs
@@ -0,0 +1,49 @@
+﻿using FsgXmk.Abstractions.Interfaces.IO;
+using Kaitai;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FsgXmk.Kaitai.IO
+{
+    public class KaitaiXmkBlobsStreamReader : IXmkBlobsReader
+    {
+        private readonly KaitaiStream _stream;
+        private readonly bool _leaveOpen;
+        private bool _disposed;
+
+        public KaitaiXmkBlobsStreamReader(KaitaiStream stream) : this(stream, false)
+        {
+        }
+
+        public KaitaiXmkBlobsStreamReader(KaitaiStream stream, bool leaveOpen)
+        {
+            _stream = stream;
+            _leaveOpen = leaveOpen;
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                if (!_leaveOpen)
+                {
+                    _stream.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public IEnumerable<string> Read(uint blobsLength)
+        {
+            var bytes = _stream.ReadBytes(blobsLength);
+            var stream = new KaitaiStream(bytes);
+            var blobs = new Xmk.XmkBlobs(stream);
+            return blobs.Values;
+        }
+
+        public Task<IEnumerable<string>> ReadAsync(uint blobsLength)
+        {
+            return Task.FromResult(Read(blobsLength));
+        }
+    }
+}
