c4d3cda6ededf7da08a8c6ee90a151818509f148
Anthony Chamberland <anthochamberland19@outlook.com>
2024-12-07 16:56:12 -0500

    Add XmkBlobsReader interfaces

---
+++ src/FsgXmk.Abstractions/Interfaces/Factories/IXmkBlobsStreamReaderFactory.cs
@@ -0,0 +1,10 @@
+﻿using FsgXmk.Abstractions.Interfaces.IO;
+using System.IO;
+
+namespace FsgXmk.Abstractions.Interfaces.Factories
+{
+    public interface IXmkBlobsStreamReaderFactory
+    {
+        IXmkBlobsReader Create(Stream stream, bool leaveOpen);
+    }
+}
---
+++ src/FsgXmk.Abstractions/Interfaces/IO/IXmkBlobsReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FsgXmk.Abstractions.Interfaces.IO
+{
+    public interface IXmkBlobsReader : IDisposable
+    {
+        IEnumerable<string> Read(uint blobsLength);
+        Task<IEnumerable<string>> ReadAsync(uint blobsLength);
+    }
+}
