fda7a7f50f6db0049e75ec55af9805d7957e5f65
Anthony Chamberland <anthochamberland19@outlook.com>
2024-12-08 12:26:10 -0500

    Add FsgXmk.Extensions.DependencyInjection project

---
+++ src/FsgXmk.Extensions.DependencyInjection/FsgXmkServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FsgXmk.Abstractions.Interfaces.Factories;
+using FsgXmk.Factories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FsgXmk.Extensions.DependencyInjection
+{
+    public static class FsgXmkServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFsgXmk(this IServiceCollection services)
+        {
+            return services.AddSingleton<IXmkHeaderByteArrayReaderFactory, XmkHeaderByteArrayReaderFactory>()
+                           .AddSingleton<IXmkTempoByteArrayReaderFactory, XmkTempoByteArrayReaderFactory>()
+                           .AddSingleton<IXmkTimeSignatureByteArrayReaderFactory, XmkTimeSignatureByteArrayReaderFactory>()
+                           .AddSingleton<IXmkEventByteArrayReaderFactory, XmkEventByteArrayReaderFactory>()
+                           .AddSingleton<IXmkBlobsByteArrayReaderFactory, XmkBlobsByteArrayReaderFactory>()
+                           .AddSingleton<IXmkHeaderStreamReaderFactory, XmkHeaderStreamReaderFactory>()
+                           .AddSingleton<IXmkTempoStreamReaderFactory, XmkTempoStreamReaderFactory>()
+                           .AddSingleton<IXmkTimeSignatureStreamReaderFactory, XmkTimeSignatureStreamReaderFactory>()
+                           .AddSingleton<IXmkEventStreamReaderFactory, XmkEventStreamReaderFactory>()
+                           .AddSingleton<IXmkBlobsStreamReaderFactory, XmkBlobsStreamReaderFactory>()
+                           .AddSingleton<IXmkStreamReaderFactory, XmkStreamReaderFactory>();
+        }
+    }
+}
