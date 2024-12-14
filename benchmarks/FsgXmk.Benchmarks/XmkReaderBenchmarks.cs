5b451bed3d493af2c73864442fa56363e0c0d31c
Anthony Chamberland <anthochamberland19@outlook.com>
2024-12-14 14:12:22 -0500

    Add FsgXmk.Benchmarks console application

---
+++ benchmarks/FsgXmk.Benchmarks/XmkReaderBenchmarks.cs
@@ -0,0 +1,62 @@
+﻿using BenchmarkDotNet.Attributes;
+using FsgXmk.Abstractions.Interfaces;
+using FsgXmk.Factories;
+using FsgXmk.Kaitai.Factories;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace FsgXmk.Benchmarks
+{
+    public class XmkReaderBenchmarks
+    {
+        private readonly string _path;
+        private readonly XmkStreamReaderFactory _factory;
+        private readonly KaitaiXmkStreamReaderFactory _kaitaiFactory;
+
+        public XmkReaderBenchmarks()
+        {
+            static string CallerFilePath([CallerFilePath] string callerFilePath = "") => callerFilePath;
+
+            _path = Path.Combine(Path.GetDirectoryName(CallerFilePath())!, "guitar_3x2.xmk");
+            _factory = new XmkStreamReaderFactory(new XmkHeaderStreamReaderFactory(new XmkHeaderByteArrayReaderFactory()),
+                                                  new XmkTempoStreamReaderFactory(new XmkTempoByteArrayReaderFactory()),
+                                                  new XmkTimeSignatureStreamReaderFactory(new XmkTimeSignatureByteArrayReaderFactory()),
+                                                  new XmkEventStreamReaderFactory(new XmkEventByteArrayReaderFactory()),
+                                                  new XmkBlobsStreamReaderFactory(new XmkBlobsByteArrayReaderFactory()));
+            _kaitaiFactory = new KaitaiXmkStreamReaderFactory();
+        }
+
+        [Benchmark(Baseline = true)]
+        public IXmk XmkStreamReader()
+        {
+            using var stream = File.OpenRead(_path);
+            using var reader = _factory.Create(stream, true);
+            return reader.Read();
+        }
+
+        [Benchmark]
+        public async Task<IXmk> XmkStreamReaderAsync()
+        {
+            using var stream = File.OpenRead(_path);
+            using var reader = _factory.Create(stream, true);
+            return await reader.ReadAsync();
+        }
+
+        [Benchmark]
+        public IXmk KaitaiXmkStreamReader()
+        {
+            using var stream = File.OpenRead(_path);
+            using var reader = _kaitaiFactory.Create(stream, true);
+            return reader.Read();
+        }
+
+        [Benchmark]
+        public async Task<IXmk> KaitaiXmkStreamReaderAsync()
+        {
+            using var stream = File.OpenRead(_path);
+            using var reader = _kaitaiFactory.Create(stream, true);
+            return await reader.ReadAsync();
+        }
+    }
+}
