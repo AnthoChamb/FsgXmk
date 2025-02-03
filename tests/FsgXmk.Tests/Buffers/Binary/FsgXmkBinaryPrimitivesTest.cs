ac7378606c6ed69559d347a68efdb1da5727132a
Anthony Chamberland <anthochamberland19@outlook.com>
2025-02-02 21:07:33 -0500

    Add Tests project

---
+++ tests/FsgXmk.Tests/Buffers/Binary/FsgXmkBinaryPrimitivesTest.cs
@@ -0,0 +1,34 @@
+﻿using FsgXmk.Buffers.Binary;
+using System;
+using Xunit;
+
+namespace FsgXmk.Tests.Buffers.Binary
+{
+    public class FsgXmkBinaryPrimitivesTest
+    {
+        [Fact]
+        public void ReadSingleBigEndian_ReturnsBigEndianValue()
+        {
+            // Arrange
+            // Act
+            var value = FsgXmkBinaryPrimitives.ReadSingleBigEndian(stackalloc byte[] { 0x42, 0x28, 0x00, 0x00 });
+
+            // Assert
+            Assert.Equal(42, value);
+        }
+
+        [Fact]
+        public void ReadSingleBigEndian_SourceLessThan4Bytes_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            // Act
+            void Act()
+            {
+                FsgXmkBinaryPrimitives.ReadSingleBigEndian(stackalloc byte[3]);
+            }
+
+            // Assert
+            Assert.Throws<ArgumentOutOfRangeException>(Act);
+        }
+    }
+}
---
+++ tests/FsgXmk.Tests/IO/Extensions/StreamExtensionsTest.cs
@@ -0,0 +1,104 @@
+﻿using FsgXmk.IO.Extensions;
+using System;
+using System.Buffers;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FsgXmk.Tests.IO.Extensions
+{
+    public class StreamExtensionsTest
+    {
+        [Fact]
+        public void ReadExactly_ReadsBytes()
+        {
+            // Arrange
+            var buffer = ArrayPool<byte>.Shared.Rent(4);
+            try
+            {
+                using (var stream = new MemoryStream(new byte[] { 1, 2, 3, 4 }, false))
+                {
+                    // Act
+                    StreamExtensions.ReadExactly(stream, buffer, 0, 4);
+                }
+
+                // Assert
+                Assert.Equal(stackalloc byte[] { 1, 2, 3, 4 }, buffer.AsSpan().Slice(0, 4));
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+
+        [Fact]
+        public void ReadExactly_BytesReadLessThanCount_ThrowsEndOfStreamException()
+        {
+            // Arrange
+            var buffer = ArrayPool<byte>.Shared.Rent(4);
+            try
+            {
+                using var stream = new MemoryStream(new byte[3], false);
+
+                // Act
+                void Act()
+                {
+                    StreamExtensions.ReadExactly(stream, buffer, 0, 4);
+                }
+
+                // Assert
+                Assert.Throws<EndOfStreamException>(Act);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+
+        [Fact]
+        public async Task ReadExactlyAsync_ReadsBytes()
+        {
+            // Arrange
+            var buffer = ArrayPool<byte>.Shared.Rent(4);
+            try
+            {
+                using (var stream = new MemoryStream(new byte[] { 1, 2, 3, 4 }, false))
+                {
+                    // Act
+                    await StreamExtensions.ReadExactlyAsync(stream, buffer, 0, 4);
+                }
+
+                // Assert
+                Assert.Equal(stackalloc byte[] { 1, 2, 3, 4 }, buffer.AsSpan().Slice(0, 4));
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+
+        [Fact]
+        public async Task ReadExactlyAsync_BytesReadLessThanCount_ThrowsEndOfStreamException()
+        {
+            // Arrange
+            var buffer = ArrayPool<byte>.Shared.Rent(4);
+            try
+            {
+                using var stream = new MemoryStream(new byte[3], false);
+
+                // Act
+                async Task Act()
+                {
+                    await StreamExtensions.ReadExactlyAsync(stream, buffer, 0, 4);
+                }
+
+                // Assert
+                await Assert.ThrowsAsync<EndOfStreamException>(Act);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+    }
+}
