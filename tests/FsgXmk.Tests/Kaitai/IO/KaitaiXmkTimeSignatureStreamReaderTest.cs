8324857a60f72fcc50eccc5786c44f034f827128
Anthony Chamberland <61058826+AnthoChamb@users.noreply.github.com>
2025-03-30 16:02:08 -0400

    Add XmkTimeSignatureStreamReader tests

---
+++ tests/FsgXmk.Tests/Kaitai/IO/KaitaiXmkTimeSignatureStreamReaderTest.cs
@@ -0,0 +1,11 @@
+﻿using FsgXmk.Abstractions.Interfaces.Factories;
+using FsgXmk.Kaitai.Factories;
+using FsgXmk.Tests.IO;
+
+namespace FsgXmk.Tests.Kaitai.IO
+{
+    public class KaitaiXmkTimeSignatureStreamReaderTest : XmkTimeSignatureStreamReaderTest
+    {
+        protected override IXmkTimeSignatureStreamReaderFactory Factory { get; } = new KaitaiXmkTimeSignatureStreamReaderFactory();
+    }
+}
